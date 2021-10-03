6abf62d019b5ee78cfe10fb91bd1646e0e3839fb
Jenna Murrell <murrelljenna@gmail.com>
2021-10-03 15:16:15 -0400

    Building menu appears / disapears with building tool

--- Assets/Scripts/Src/UI/Controllers/BuildingMenuController.cs
+++ Assets/Scripts/Src/UI/Controllers/BuildingMenuController.cs
@@ -1,14 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static game.assets.utilities.GameUtils.MagicWords;
 
-public class BuildingMenuController
+public class BuildingMenuController : MonoBehaviour
 {
-    private GameObject rootGameObj;
+    private GameObject buildingMenu;
 
-    public BuildingMenuController()
+    void Start()
     {
-        rootGameObj = UI.BuildingMenu();
+        buildingMenu = GameObject.Find(GameObjectNames.BuildingMenu);
+    }
+
+    void OnDisable()
+    {
+        buildingMenu.SetActive(false);
+    }
+
+    void OnEnable()
+    {
+        if (buildingMenu != null)
+        { // Resulting in exception at beginning of game unless inside this statement
+            buildingMenu.SetActive(true);
+        }
+    }
+
+    public void triggerButtonPush(int num)
+    {
         
     }
 }

149a9978eaaabe0ed5ac386b7805b6006abd8f61
Jenna Murrell <murrelljenna@gmail.com>
2021-10-11 10:27:41 -0400

    Added unit guard behaviour
    Unit will fight units within a certain radius but avoid leaving that
    radius.

---
+++ Assets/Scripts/Src/Behaviours/Guard.cs
@@ -0,0 +1,100 @@
+﻿using game.assets.ai;
+using game.assets.utilities;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Attack))]
+[RequireComponent(typeof(Movement))]
+public class Guard : MonoBehaviour
+{
+    private Attack attack;
+    private Movement movement;
+
+    private Vector3 pointToGuard;
+    private float radiusToGuard;
+    private int lastNoEnemies = 0;
+
+    private Health intruder;
+
+    void Start()
+    {
+        attack = GetComponent<Attack>();
+        movement = GetComponent<Movement>();
+    }
+
+    public void guard(Vector3 point, float radius)
+    {
+        pointToGuard = point;
+        radiusToGuard = radius;
+        movement.goTo(point);
+        movement.reachedDestination.AddListener(startGuard);
+    }
+
+    private void startGuard()
+    {
+        InvokeRepeating("guardInRange", 1f, 1f);
+    }
+
+    private void guardInRange()
+    {
+        Health[] units = GameUtils.findEnemyUnitsInRange(pointToGuard, radiusToGuard);
+        if (units.Length == lastNoEnemies)
+        {
+            return;
+        }
+
+        lastNoEnemies = units.Length;
+
+        Health candidateEnemy = firstWithReasonablePath(units);
+        if (candidateEnemy != null)
+        {
+            attack.attack(candidateEnemy);
+            intruder = candidateEnemy;
+        }
+        else
+        {
+            movement.goTo(pointToGuard);
+        }
+    }
+
+    private Health firstWithReasonablePath(Health[] units)
+    {
+        for (int i = 0; i < units.Length; i++)
+        {
+            if (units[i].IsEnemyOf(this) && units[i].HP > 0)
+            {
+                float dist = movement.pathLength(units[i].GetComponent<Transform>().position);
+                if (isInRange(units[i].GetComponent<Health>()) || dist < 3f)
+                {
+                    return units[i];
+                }
+            }
+        }
+
+        return null;
+    }
+
+    private bool isInRange(Health thingToAttack)
+    {
+        Vector3 closestPoint = thingToAttack.gameObject.GetComponent<Collider>().ClosestPointOnBounds(this.gameObject.transform.position);
+
+        float deltaX = this.gameObject.transform.position.x - closestPoint.x;
+        float deltaZ = this.gameObject.transform.position.z - closestPoint.z;
+        float distance = Mathf.Sqrt(deltaX * deltaX + deltaZ * deltaZ);
+
+        return (distance < attack.attackRange);
+    }
+
+    private void cancelOrdersAndReturnToGuard()
+    {
+        attack.cancelOrders();
+        movement.goTo(pointToGuard);
+    }
+
+    public void cancel()
+    {
+        CancelInvoke();
+        lastNoEnemies = 0;
+    }
+}
---
+++ Assets/Scripts/Test/Play Mode/Behaviour Tests/TestGuard.cs
@@ -0,0 +1,94 @@
+﻿using game.assets.ai;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+using NUnit.Framework;
+using game.assets;
+
+public class TestGuard
+{
+    private const string sceneName = "TestGuard.unity";
+    private Guard guard;
+    private Health intruder;
+
+    private Vector3 pointInRadius;
+    private Vector3 pointOutsideRadius;
+    private Vector3 guardCenter;
+
+    public void putIntruderInRadius(Health intruder)
+    {
+        intruder.gameObject.transform.position = pointInRadius;
+    }
+
+    public void putIntruderOutsideRadius(Health intruder)
+    {
+        intruder.gameObject.transform.position = pointOutsideRadius;
+    }
+
+    private bool attackeeTookDamage = false;
+    public void onTookDamageCallback(float hp, float maxHp)
+    {
+        attackeeTookDamage = true;
+    }
+
+    [UnitySetUp]
+    public IEnumerator SetUp()
+    {
+        EditorSceneManager.LoadSceneInPlayMode(
+            Test.TestUtils.testSceneDirPath + sceneName,
+            new LoadSceneParameters(LoadSceneMode.Single)
+        );
+
+        yield return null;
+
+        guard = GameObject.Find("Guard").GetComponent<Guard>();
+        intruder = GameObject.Find("Intruder").GetComponent<Health>();
+
+        guard.SetAsMine();
+        intruder.SetAsPlayer(LocalGameManager.Get().barbarianPlayer);
+
+        pointInRadius = GameObject.Find("InsideRadius").transform.position;
+        pointOutsideRadius = GameObject.Find("OutsideRadius").transform.position;
+        guardCenter = GameObject.Find("GuardCenter").transform.position;
+
+        attackeeTookDamage = false;
+
+        yield return new EnterPlayMode();
+    }
+
+    [UnityTest, Order(1)]
+    public IEnumerator testIntruderAttackedInsideRadius()
+    {
+        guard.guard(guardCenter, 5f);
+        intruder.onLowerHP.AddListener(onTookDamageCallback);
+        putIntruderInRadius(intruder);
+        yield return new WaitForSeconds(3);
+        Assert.True(attackeeTookDamage);
+    }
+
+    [UnityTest, Order(2)]
+    public IEnumerator testIntruderNotAttackedOutsideRadius()
+    {
+        guard.guard(guardCenter, 2f);
+        intruder.onLowerHP.AddListener(onTookDamageCallback);
+        putIntruderOutsideRadius(intruder);
+        yield return new WaitForSeconds(3);
+        Assert.False(attackeeTookDamage);
+    }
+
+    [UnityTest, Order(1)]
+    public IEnumerator testGuardReturnsToPointWhenIntruderLeaves()
+    {
+        guard.guard(guardCenter, 5f);
+        intruder.onLowerHP.AddListener(onTookDamageCallback);
+        putIntruderInRadius(intruder);
+        yield return new WaitForSeconds(3);
+        Assert.True(attackeeTookDamage);
+        putIntruderOutsideRadius(intruder);
+        yield return new WaitForSeconds(3);
+        Assert.True(Vector3.Distance(guard.transform.position, guardCenter) < 0.2f);
+    }
+}

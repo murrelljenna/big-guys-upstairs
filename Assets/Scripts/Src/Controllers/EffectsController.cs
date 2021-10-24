b0a829e7e11846fb1b92ad9c6b5c7ca849837f65
Jenna Murrell <murrelljenna@gmail.com>
2021-10-24 18:40:08 -0400

    Building damage sounds

--- Assets/Scripts/Src/Controllers/EffectsController.cs
+++ Assets/Scripts/Src/Controllers/EffectsController.cs
@@ -1,9 +1,23 @@
-﻿using System.Collections;
+﻿using game.assets.ai;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EffectsController : MonoBehaviour
 {
+    private const int DEFAULT_COUNT = 30;
+    public void PlayDestroyEffects(Health health)
+    {
+        ParticleSystem[] potentialParticleSystem = Get("DestroyEffects");
+        if (potentialParticleSystem == null)
+        {
+            Debug.LogError("Trying to run particleSystem that is null");
+            return;
+        }
+        ParticleSystem particles = potentialParticleSystem[Random.Range(0, potentialParticleSystem.Length)];
+        particles.Emit(DEFAULT_COUNT);
+    }
+
     public void PlayRandom(string name, int count)
     {
         ParticleSystem[] potentialParticleSystem = Get(name);

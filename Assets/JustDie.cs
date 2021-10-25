c74ffc38630c999c2226438fd80d35bc1e3803f2
Jenna Murrell <murrelljenna@gmail.com>
2021-10-24 21:28:02 -0400

    Give units death animation

---
+++ Assets/JustDie.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class JustDie : MonoBehaviour
+{
+    [Tooltip("List of animation names, one of which will be play randomly")]
+    public string[] animationNames; // Pass in a list of fucking names apparently
+    
+    void Start()
+    {
+        Animator anim = GetComponent<Animator>();
+        // Is this seriously the best API Unity could think of for this fucking component?
+        // No method to get a list of all the animations to play one randomly. No. We are going
+        // to make you literally pass a string naming the fucking animation... filename? 
+        // Seriously who thought this was a good idea? There is literally NO METHOD that 
+        // simply returns AnimationClip[]. I can get a single AnimationClip if I PASS
+        // A FUCKING STRING representing SOMETHING that identifies the clip, apparently,
+        // but NOTHING that will just give me ALL CLIPS.
+        // So fucking stupid.
+        string name = animationNames[Random.Range(0, animationNames.Length)];
+        Debug.Log(name);
+        anim.Play(name); // Fuck you
+    }
+}
--- Assets/Scripts/Src/Spawners/Destruction/Destroy.cs
+++ Assets/Scripts/Src/Spawners/Destruction/Destroy.cs
@@ -1,11 +1,19 @@
-﻿using System.Collections;
+﻿using game.assets;
+using game.assets.player;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Destroy : MonoBehaviour
 {
+    public GameObject leaveBehind;
     public void destroy()
     {
+        if (leaveBehind != null)
+        {
+            game.assets.player.Player player = GetComponent<Ownership>().owner;
+            InstantiatorFactory.getInstantiator(false).InstantiateAsPlayer(leaveBehind, transform.position, transform.rotation, player);
+        }
         Destroy(gameObject);
     }
 }

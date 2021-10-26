375953acab9864d71bb951772e545cdac67da83f
Jenna Murrell <murrelljenna@gmail.com>
2021-10-25 20:47:55 -0400

    Adjust BuildingColourController to just fix any renderer

--- Assets/Scripts/Src/UI/Controllers/BuildingColourController.cs
+++ Assets/Scripts/Src/UI/Controllers/BuildingColourController.cs
@@ -8,10 +8,26 @@ namespace game.assets.ui
 {
     public class BuildingColourController : MonoBehaviour, IColourController
     {
+        private string[] names = new string[] {
+            "Model",
+            "Construction_0",
+            "Construction_1",
+            "Destruction_0",
+            "Destruction_1"
+        };
+
         public void SetColourToPlayer(player.Player player)
         {
+            List<string> listNames = new List<string>(names);
             string colorName = player.colour.name;
-            transform.Find("Model").gameObject.GetComponent<MeshRenderer>().material.SetTexture("_MainTex", (Resources.Load("TT_RTS_Buildings_" + colorName) as Texture));
+            Renderer[] renderers = GetComponentsInChildren<Renderer>();
+
+            for (int i = 0; i < renderers.Length; i++)
+            {
+                if (listNames.Contains(renderers[i].gameObject.name)) {
+                    renderers[i].material.SetTexture("_MainTex", (Resources.Load("TT_RTS_Buildings_" + colorName) as Texture));
+                }
+            }
         }
     }
 }

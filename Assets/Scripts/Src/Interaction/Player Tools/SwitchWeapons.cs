cd1ac197527d2ac86bd29579a0c9086c94c0577c
Jenna Murrell <murrelljenna@gmail.com>
2022-11-12 15:49:44 -0500

    Meter spammy Q input

--- Assets/Scripts/Src/Interaction/Player Tools/SwitchWeapons.cs
+++ Assets/Scripts/Src/Interaction/Player Tools/SwitchWeapons.cs
@@ -10,6 +10,10 @@ namespace game.assets.player
         [Networked(OnChanged=nameof(SelectWeaponsCallback))]
         public uint selectedTool { get; set; } = 0u;
 
+        private const float BUFFER_BETWEEN_PRESSES = 0.2f;
+
+        private float lastPressed;
+
         public override void Spawned()
         {
             selectWeapons();
@@ -21,6 +25,14 @@ namespace game.assets.player
             {
                 if (input.IsDown(PlayerNetworkInput.BUTTON_ACTION2))
                 {
+                    float lastPress = Time.time - lastPressed;
+
+                    if (lastPress < BUFFER_BETWEEN_PRESSES)
+                    {
+                        return;
+                    }
+
+                    lastPressed = Time.time;
                     Debug.Log("Is down!");
                     if (selectedTool >= transform.childCount - 1)
                     {

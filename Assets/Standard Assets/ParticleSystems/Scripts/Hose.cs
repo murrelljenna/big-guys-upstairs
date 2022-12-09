45ccd7c8b7a471baa0b8a646a80bee7f64e7b263
Jenna Murrell <murrelljenna@gmail.com>
2022-12-09 12:52:11 -0500

    Polish waterfall in two player map

--- Assets/Standard Assets/ParticleSystems/Scripts/Hose.cs
+++ Assets/Standard Assets/ParticleSystems/Scripts/Hose.cs
@@ -12,25 +12,14 @@ namespace UnityStandardAssets.Effects
         public ParticleSystem[] hoseWaterSystems;
         public Renderer systemRenderer;
 
-        private float m_Power;
-
 
         // Update is called once per frame
         private void Update()
         {
-            m_Power = Mathf.Lerp(m_Power, Input.GetMouseButton(0) ? maxPower : minPower, Time.deltaTime*changeSpeed);
-
-            if (Input.GetKeyDown(KeyCode.Alpha1))
-            {
-                systemRenderer.enabled = !systemRenderer.enabled;
-            }
 
             foreach (var system in hoseWaterSystems)
             {
-				ParticleSystem.MainModule mainModule = system.main;
-                mainModule.startSpeed = m_Power;
-                var emission = system.emission;
-                emission.enabled = (m_Power > minPower*1.1f);
+                //system.main.simulationSpeed = 0.15f;
             }
         }
     }

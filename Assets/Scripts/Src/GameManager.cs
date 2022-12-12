563a02770e5e138559132eae8eb98a77ae49349d
Jenna Murrell <murrelljenna@gmail.com>
2022-12-11 19:22:54 -0500

    Bump to 0.2.0-alpha

--- Assets/Scripts/Src/GameManager.cs
+++ Assets/Scripts/Src/GameManager.cs
@@ -10,7 +10,7 @@ namespace game.assets
 {
     public abstract class GameManager : MonoBehaviour
     {
-        public const string version = "0.1.1-alpha";
+        public const string version = "0.2.0-alpha";
 
         public enum GameMode
         {

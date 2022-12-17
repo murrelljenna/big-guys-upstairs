f37134adbba75d08586343953ef435bdc25942e0
Jenna Murrell <murrelljenna@gmail.com>
2022-12-17 11:52:36 -0500

    Move host game / join game to different menus

--- Assets/Scripts/Src/UI/MainMenu.cs
+++ Assets/Scripts/Src/UI/MainMenu.cs
@@ -10,6 +10,16 @@ public class MainMenu : MonoBehaviour
 
     private string enteredSessionName = "TestRoom";
 
+    private PageState pageState = PageState.Main;
+
+    private enum PageState
+    {
+        Main,
+        Multiplayer,
+        HostGame,
+        JoinGame
+    }
+
     private void Start()
     {
         gameManager = GetComponent<NetworkedGameManager>();
@@ -29,20 +39,69 @@ public class MainMenu : MonoBehaviour
             var oldColor = GUI.backgroundColor;
             GUI.Box(new Rect(xCenter - buttonWidth / 2 - padding, yCenter - buttonHeight - buttonHeight / 2 - padding * 2, buttonWidth + 2 * padding, buttonHeight * 4 + padding * 4 + 7), "");
 
-            if (GUI.Button(new Rect(xCenter - buttonWidth / 2, yCenter - buttonHeight - padding - buttonHeight / 2, buttonWidth, buttonHeight), "Host"))
+
+            if (pageState == PageState.Main)
             {
-                gameManager.InitGame("FourPlayer", enteredSessionName);
+                if (GUI.Button(new Rect(xCenter - buttonWidth / 2, yCenter - buttonHeight / 2, buttonWidth, buttonHeight), "Multiplayer"))
+                {
+                    OpenMultiplayerMenu();
+                }
+                if (GUI.Button(new Rect(xCenter - buttonWidth / 2, yCenter + buttonHeight * 2, buttonWidth, buttonHeight), "Quit Game"))
+                {
+                    gameManager.Quit();
+                }
             }
-            if (GUI.Button(new Rect(xCenter - buttonWidth / 2, yCenter - buttonHeight / 2, buttonWidth, buttonHeight), "Join"))
+            else if (pageState == PageState.Multiplayer)
             {
-                gameManager.JoinGame(enteredSessionName, "FourPlayer");
+                if (GUI.Button(new Rect(xCenter - buttonWidth / 2, yCenter - buttonHeight - padding - buttonHeight / 2, buttonWidth, buttonHeight), "Host New Game"))
+                {
+                    OpenHostGameMenu();
+                }
+                if (GUI.Button(new Rect(xCenter - buttonWidth / 2, yCenter - buttonHeight / 2, buttonWidth, buttonHeight), "Find Games"))
+                {
+                    OpenJoinGameMenu();
+                }
             }
-            if (GUI.Button(new Rect(xCenter - buttonWidth / 2, yCenter + buttonHeight * 2, buttonWidth, buttonHeight), "Quit Game"))
+            else if (pageState == PageState.JoinGame)
             {
-                gameManager.Quit();
+                GUI.Label(new Rect(xCenter - buttonWidth / 2 + 40, yCenter - buttonHeight / 2 + buttonHeight + padding, buttonWidth, buttonHeight), new GUIContent("Enter server name to join:"));
+                enteredSessionName = GUI.TextField(new Rect(xCenter - buttonWidth / 2, yCenter - buttonHeight / 2 + buttonHeight + padding * 3.5f, buttonWidth, buttonHeight), enteredSessionName, 64, textStyle);
+
+                if (GUI.Button(new Rect(xCenter - buttonWidth / 2, yCenter - buttonHeight / 2, buttonWidth, buttonHeight), "Join"))
+                {
+                    gameManager.JoinGame(enteredSessionName, "FourPlayer");
+                }
             }
-            GUI.Label(new Rect(xCenter - buttonWidth / 2 + 40, yCenter - buttonHeight / 2 + buttonHeight + padding, buttonWidth, buttonHeight), new GUIContent("Enter a room to join:"));
-            enteredSessionName = GUI.TextField(new Rect(xCenter - buttonWidth / 2, yCenter - buttonHeight / 2 + buttonHeight + padding * 3.5f, buttonWidth, buttonHeight), enteredSessionName, 64, textStyle);
+            else if (pageState == PageState.HostGame)
+            {
+
+                GUI.Label(new Rect(xCenter - buttonWidth / 2 + 40, yCenter - buttonHeight / 2 + buttonHeight + padding, buttonWidth, buttonHeight), new GUIContent("Enter server name:"));
+                enteredSessionName = GUI.TextField(new Rect(xCenter - buttonWidth / 2, yCenter - buttonHeight / 2 + buttonHeight + padding * 3.5f, buttonWidth, buttonHeight), enteredSessionName, 64, textStyle);
+                if (GUI.Button(new Rect(xCenter - buttonWidth / 2, yCenter - buttonHeight - padding - buttonHeight / 2, buttonWidth, buttonHeight), "Start Server"))
+                {
+                    gameManager.InitGame("FourPlayer", enteredSessionName);
+                }
+            }
         }
+    }
+
+    private void OpenMainMenu()
+    {
+        pageState = PageState.Main;
+    }
+
+    private void OpenMultiplayerMenu()
+    {
+        pageState = PageState.Multiplayer;
+    }
+
+    private void OpenHostGameMenu()
+    {
+        pageState = PageState.HostGame;
+    }
+
+    private void OpenJoinGameMenu()
+    {
+        pageState = PageState.JoinGame;
     }
 }

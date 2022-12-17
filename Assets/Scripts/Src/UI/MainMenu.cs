658712793671e24ceacbedc52d48f100e57e6735
Jenna Murrell <murrelljenna@gmail.com>
2022-12-17 12:55:09 -0500

    Add selectable scenes when starting a game

--- Assets/Scripts/Src/UI/MainMenu.cs
+++ Assets/Scripts/Src/UI/MainMenu.cs
@@ -52,6 +52,39 @@ public class MainMenu : MonoBehaviour
         }
     }
 
+    public class GameMap
+    {
+        public string sceneName;
+        public int maxPlayers;
+
+        public GameMap(string sceneName, int maxPlayers) {
+            this.sceneName = sceneName;
+            this.maxPlayers = maxPlayers;
+        }
+
+        public static GameMap[] AllMaps()
+        {
+            return new GameMap[] {
+                new GameMap("FourPlayer", 4),
+                new GameMap("TwoPlayer", 2)
+            };
+        }
+
+        public static string[] AllMapNames()
+        {
+            string[] mapNames = new string[AllMaps().Length];
+
+            for (int i = 0; i < AllMaps().Length; i++)
+            {
+                mapNames[i] = AllMaps()[i].sceneName;
+            }
+
+            return mapNames;
+        }
+    }
+
+    private int selectedScene = 0;
+
     private void OnGUI()
     {
         if (gameManager._runner == null)
@@ -112,12 +145,17 @@ public class MainMenu : MonoBehaviour
             }
             else if (pageState == PageState.HostGame)
             {
+                selectedScene = GUI.SelectionGrid(
+                    new Rect(xCenter - buttonWidth / 2, yCenter - buttonHeight - buttonHeight / 2 - padding, buttonWidth, buttonHeight * 2 + padding),
+                    selectedScene, 
+                    GameMap.AllMapNames(),
+                    1);
                 GUI.Box(new Rect(xCenter - buttonWidth / 2 - padding, yCenter - buttonHeight - buttonHeight / 2 - padding * 2, buttonWidth + 2 * padding, buttonHeight * 4 + padding * 4 + 7), "");
                 GUI.Label(new Rect(xCenter - buttonWidth / 2 + 40, yCenter - buttonHeight / 2 + buttonHeight + padding, buttonWidth, buttonHeight), new GUIContent("Enter server name:"));
                 enteredSessionName = GUI.TextField(new Rect(xCenter - buttonWidth / 2, yCenter - buttonHeight / 2 + buttonHeight + padding * 3.5f, buttonWidth, buttonHeight), enteredSessionName, 64, textStyle);
                 if (GUI.Button(new Rect(xCenter - buttonWidth / 2, yCenter + buttonHeight * 2, buttonWidth, buttonHeight), "Start Server"))
                 {
-                    gameManager.InitGame("FourPlayer", enteredSessionName);
+                    gameManager.InitGame(GameMap.AllMaps()[selectedScene].sceneName, enteredSessionName);
                 }
 
                 if (GUI.Button(new Rect(xCenter - buttonWidth / 2, yCenter + buttonHeight * 3 + padding * 1.5f, buttonWidth / 4, buttonHeight), "Back"))
